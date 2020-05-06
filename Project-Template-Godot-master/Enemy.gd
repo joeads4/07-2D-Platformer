0c56264d1b7e0d328f80531417669be2504a1abd
Jocelyn <joeads@iu.edu>
2020-05-05 20:12:21 -0400

    feat: Enemy
    Made enemy that walks back and forth, and can detect the floor and walls

---
+++ Project-Template-Godot-master/Enemy.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+const GRAVITY = 10
+const SPEED = 30
+const FLOOR = Vector2(0, -1)
+
+var velocity = Vector2()
+var direction = 1
+
+
+
+func _physics_process(delta):
+	velocity.x = SPEED * direction
+	
+	if direction == 1:
+		$AnimatedSprite.flip_h = false
+	else:
+		$AnimatedSprite.flip_h = true
+	
+	$AnimatedSprite.play("Walk")
+	
+	velocity.y += GRAVITY
+	
+	velocity = move_and_slide(velocity, FLOOR)
+	
+	if is_on_wall():
+		direction = direction * -1
+		$RayCast2D.position.x *= -1
+	if $RayCast2D.is_colliding() == false:
+		direction = direction * -1
+		$RayCast2D.position.x *= -1
